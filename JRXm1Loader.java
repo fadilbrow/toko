09a4690f10e597505dfffbe93d8a34553c5e5fda
Fadil <55827931+fadilbrow@users.noreply.github.com>
2021-07-27 20:40:24 +0700

    Add files via upload

---
+++ JRXm1Loader.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toko.view;
+
+import java.io.File;
+import net.sf.jasperreports.engine.design.JasperDesign;
+
+/**
+ *
+ * @author Asus
+ */
+class JRXm1Loader {
+
+    static JasperDesign load(File report) {
+        
+        //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
